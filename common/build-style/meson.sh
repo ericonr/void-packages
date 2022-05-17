105fc9def2cd188bb30e100a67e6c9f550275e04
Érico Nogueira <erico.erc@gmail.com>
2022-05-17 12:35:40 -0300

    build-style/meson: update flags.
    - disable LTO, was breaking static libraries
    - enable PIE for executables as well

--- common/build-style/meson.sh
+++ common/build-style/meson.sh
@@ -120,8 +120,8 @@ do_configure() {
 		--buildtype=plain \
 		--auto-features=auto \
 		--wrap-mode=nodownload \
-		-Db_lto=true -Db_ndebug=true \
-		-Db_staticpic=true \
+		-Db_lto=false -Db_ndebug=true \
+		-Db_staticpic=true -Db_pie=true \
 		${configure_args} . ${meson_builddir}
 }
 
